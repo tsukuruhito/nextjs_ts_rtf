dddb4028df830b291949e57bf32849f6e729078c
Tsuku <ts.createlink@gmail.com>
2023-03-03 00:36:39 +0900

    change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,28 +20,10 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
           <a href="/rtf" className={styles.card}>
             <h3>rtf &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              only canvas and spheregeometory
             </p>
           </a>
         </div>
